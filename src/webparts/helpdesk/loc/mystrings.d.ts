b44d91090fcc4c5463bb94f641597fef09c074a4
Victor Julian Candia Rivas <victorjc.rivas.tech@outlook.com>
2025-12-18 00:43:01 -0600

    Projecto SPFx inicial

---
+++ src/webparts/helpdesk/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHelpdeskWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'HelpdeskWebPartStrings' {
+  const strings: IHelpdeskWebPartStrings;
+  export = strings;
+}
